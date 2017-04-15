6962dac9ee1ae9e22245e1347e946b35bffac30d
yanlind <yanlind@andrew.cmu.edu>
2017-04-14 21:05:59 -0400

    Initial Commit

---
+++ CRawlerPlusPlus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CRawlerPlusPlus
+//
+//  Created by Yanlin Duan on 4/14/17.
+//  Copyright © 2017 Yanlin Duan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
